4707a30781acc3b8bab6bd2dc42c0659bbfac5e6
bettyspurgeon <betty.spurgeon713@gmail.com>
2021-12-06 09:59:10 +0100

    functions for map toggle

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const mapbutton = document.querySelectorAll(".map-btn")
+const campusButton = document.querySelector(".campus");
+const cityButton = document.querySelector(".city-centre")
+const mapDiv = document.querySelector("#maps")
 
+campusButton.addEventListener("click", function() {
+    mapDiv.classList.toggle("campus-map")
+})
+
+cityButton.addEventListener("click", function() {
+    mapDiv.classList.toggle("city-map")
+})
